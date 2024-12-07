98e97092823cda4a3711ed9967c243ad71c25974
Ana Monteiro <ana.margarida.3m@gmail.com>
2024-12-07 11:26:15 +0000

    adaptar o php

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>rent-a-car</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="../css/frota.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -20,7 +20,7 @@
     <div class="menu">
         Menu
         <div class="menu-options">
-            <a class="menu-option" href="frota.php">Frota</a>
+            <a class="menu-option" href="../frota.php">Frota</a>
             <a class="menu-option" href="quemsomos.html">Quem somos</a>
             <a class="menu-option" href="reservas.html">Reservas</a>
             <a class="menu-option" href="reviews.html">Reviews</a>
